a6abfebb35bde9c4eb82d52f7a06f9a81dc4e6f6
andre <wijayabagusandre@gmail.com>
2022-11-05 10:09:53 +0700

    edit date

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./index')
-
+process.env.TZ = 'Asia/Jakarta'; 
 port = 3000;
 const server = http.createServer(app);
 server.listen(process.env.port || port,() =>{
