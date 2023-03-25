901bc49de66fb09806696c328e4af004a2f5c786
Bagus Andre Wijaya <103176578+bagusandrewijaya@users.noreply.github.com>
2023-03-25 13:30:05 +0700

    Update database.js

--- middleware/database.js
+++ middleware/database.js
@@ -1,13 +1,11 @@
 const mysql = require('mysql2')
 
 var connectdb = mysql.createConnection({
-    host:"ap-southeast.connect.psdb.cloud",
-    user:"65wpldvwt3m3l4qp9zci",
-    password:"pscale_pw_bgnRx3mPAHZMKPdcznJ6jQuZNqRPq3n2s289Ed9dOGA",
-database:"pmbsites",
-    ssl:  {
-    rejectUnauthorized: false
-}
+    host:"yourhost",
+    user:"user",
+    password:"somepass",
+database:"somedb",
+
 });
 connectdb.connect((err)=>{
     if (!err) {
